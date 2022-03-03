b713107a14ece9249bff289a782e545e7b66cad5
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-03-02 19:25:11 -0500

    initialization

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ProTip from "../src/ProTip";
+import Link from "../src/Link";
+import Navbar from "../src/navbar/Navbar";
+
+export default function Index() {
+  return (
+    <Navbar></Navbar>
+    // <Container maxWidth="sm">
+    //   <Box sx={{ my: 4 }}>
+    //     <Typography variant="h1" component="h1" gutterBottom>
+    //       Next.js example
+    //     </Typography>
+    //     <Link href="/about" color="secondary">
+    //       Go to the about page
+    //     </Link>
+    //   </Box>
+    // </Container>
+  );
+}
